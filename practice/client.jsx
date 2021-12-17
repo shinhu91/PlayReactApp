f6e3e91f2b5f6e3cbf3f26b0598a24fb2b15db43
DESKTOP-ER9PP0O\82102 <shinhu91@gmail.com>
2021-12-17 21:55:17 +0900

    가위바위보 클래스 컴포넌트

--- practice/client.jsx
+++ practice/client.jsx
@@ -1,6 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ResponseCheck from './4.responseCheck/ResponseCheck';
+import React from "react"
+import ReactDOM from "react-dom"
+
+import ResponseCheck from './4.responseCheck/ResponseCheck' // 반응속도 체크
+import Rsp from './5.RSP/Rsp' // 가위 바위 보
 
 
-ReactDOM.render(<ResponseCheck />, document.querySelector("#root"))
+
+ReactDOM.render(<Rsp />, document.querySelector("#root"))
