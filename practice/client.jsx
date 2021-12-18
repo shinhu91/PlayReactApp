0bda2edc2d8ca59772bd2910217ef4f6c3881107
DESKTOP-ER9PP0O\82102 <shinhu91@gmail.com>
2021-12-18 16:51:14 +0900

    로또번호 추출 class, function 컴포넌트 useMemo, useCallback 사용

--- practice/client.jsx
+++ practice/client.jsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom"
 
 import ResponseCheck from './4.responseCheck/ResponseCheck' // 반응속도 체크
 import Rsp from './5.RSP/Rsp' // 가위 바위 보
+import Lotto from './6.Lotto/Lotto' // 로또
 
 
 
-ReactDOM.render(<Rsp />, document.querySelector("#root"))
+ReactDOM.render(<Lotto />, document.querySelector("#root"))
